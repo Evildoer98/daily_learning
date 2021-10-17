56ec7ad059348bd05db0f5bb19286b4003ee7680
徐世成 <1213089093@qq.com>
2021-10-17 23:23:44 +0800

    day05——promise的try、catch的同步、异步的异常捕获

--- day05——promise的try、catch的同步、异步的异常捕获.js/day_05.md
+++ day05——promise的try、catch的同步、异步的异常捕获.js/day_05.md
@@ -1,8 +1,12 @@
-/*
+<!--
  * @Descripttion: 
  * @version: 
  * @Author: Evildoer98
- * @Date: 2021-10-17 23:13:14
+ * @Date: 2021-10-17 23:14:37
  * @LastEditors: Evildoer98
- * @LastEditTime: 2021-10-17 23:13:20
- */
+ * @LastEditTime: 2021-10-17 23:22:21
+-->
+
+1. 进程
+2. 主线程
+3. 任务队列
--- day05——promise的try、catch的同步、异步的异常捕获.js/demo_05.js
+++ day05——promise的try、catch的同步、异步的异常捕获.js/demo_05.js
@@ -4,5 +4,30 @@
  * @Author: Evildoer98
  * @Date: 2021-10-17 23:13:32
  * @LastEditors: Evildoer98
- * @LastEditTime: 2021-10-17 23:13:32
+ * @LastEditTime: 2021-10-17 23:19:17
  */
+
+// 普通的、同步的异常捕获
+try {
+    thorw new Error('Evildoer')
+} catch (err) {
+    console.log(err);
+}
+
+
+// 异步 promise的异步的异常捕获
+async function fn1 () {
+    let n = Promise.reject('666')
+    console.log(n);   // err
+
+    try {
+        await n    // 不加 await 则无法捕获
+        console.log(n); 
+    } catch (err) {
+        console.log('err' + err);
+    } finally {
+        
+    }
+}
+
+fn1()
