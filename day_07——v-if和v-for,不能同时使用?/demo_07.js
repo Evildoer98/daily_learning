3b718ad24cc600166cb0cb1bdf430d3ca6b53bc6
徐世成 <1213089093@qq.com>
2021-10-18 21:23:32 +0800

    day_07——v-if和v-for示例

--- day_07——v-if和v-for,不能同时使用?/demo_07.js
+++ day_07——v-if和v-for,不能同时使用?/demo_07.js
@@ -4,6 +4,12 @@
  * @Author: Evildoer98
  * @Date: 2021-10-18 21:14:09
  * @LastEditors: Evildoer98
- * @LastEditTime: 2021-10-18 21:14:10
+ * @LastEditTime: 2021-10-18 21:17:38
  */
 
+new Vue ({
+    el: '#app',
+    data: {
+        arr: ['Hello', 'My', 'Name', 'is', 'Evildoer']
+    }
+})
