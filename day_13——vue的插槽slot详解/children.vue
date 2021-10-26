d1f10ee55357fb83cf989406c74cb664a47a67a4
徐世成 <1213089093@qq.com>
2021-10-26 22:37:24 +0800

    feat: day_13————vue插槽详解 demo.vue and day_13.md

---
+++ day_13——vue的插槽slot详解/children.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Evildoer98
+ * @Date: 2021-10-26 22:24:27
+ * @LastEditors: Evildoer98
+ * @LastEditTime: 2021-10-26 22:32:55
+-->
+
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <slot></slot>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'children',
+  data () {
+    return {
+      msg: '子组件'
+    }
+  }
+}
+
+</script>
---
+++ day_13——vue的插槽slot详解/demo.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Evildoer98
+ * @Date: 2021-10-26 22:20:38
+ * @LastEditors: Evildoer98
+ * @LastEditTime: 2021-10-26 22:27:12
+-->
+
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <chidlren>
+      <p>Hello World</p>
+    </chidlren>
+  </div>
+
+</template>
+
+<script>
+import children from './children'
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'vue 插槽 slot - 父组件'
+    }
+  },
+  components: {children}
+}
+
+</script>
+
