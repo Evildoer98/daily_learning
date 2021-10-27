51b6845ab806dc4978537c21c1e09788cc51046c
徐世成 <1213089093@qq.com>
2021-10-27 21:18:12 +0800

    feat: vue的mixins

---
+++ day_14——vue的mixins/demo.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Evildoer98
+ * @Date: 2021-10-27 21:03:55
+ * @LastEditors: Evildoer98
+ * @LastEditTime: 2021-10-27 21:13:27
+-->
+
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h1>{{name}}</h1>
+    <h1>{{age}}</h1>
+    <h1>{{msg}}</h1>
+    <button @click="fn">test</button>
+  </div>
+</template>
+
+<script>
+import {mixinsObj} from './mixins/mixinsObj'
+export default {
+  name: 'demo',
+  mixins: [mixinsObj],
+  data () {
+    return {
+      msg: '1111',
+      age: 12
+    }
+  }
+}
+</script>
