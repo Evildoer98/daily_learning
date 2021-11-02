8ffba438bb8ade75a4e3b82f7878341f46ce24fe
徐世成 <1213089093@qq.com>
2021-11-02 23:56:04 +0800

    day_18————vue兄弟组件组件通行

---
+++ day_18——vue兄弟组件之间通信/a_item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+    <p>
+      <button @click='btn'>向b发送信息</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import eventUtils from './utils/eventUtils'
+export default {
+  name: 'a_item',
+  data () {
+    return {
+      msg: 'aaa'
+    }
+  },
+  methods: {
+    btn() {
+      console.log('123');
+      eventUtils.$emit('infoEvent', 'Evildoer98')
+    }
+  }
+  
+}
+</script>
