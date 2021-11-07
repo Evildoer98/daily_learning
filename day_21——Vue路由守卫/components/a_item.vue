d1fd3b2f496670897594ea414e4646c82db0155c
徐世成 <1213089093@qq.com>
2021-11-07 22:48:24 +0800

    day_21————Vue路由守卫

---
+++ day_21——Vue路由守卫/components/a_item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'a_item',
+  data () {
+    return {
+      msg: 'aaa'
+    }
+  },
+
+  // 进入路由
+  beforeRouteEnter (to, from, next) {
+    next(false)
+  },
+
+  // 离开路由
+  beforeRouteLeave (to, from, next) {
+    next(true)
+  }
+  
+}
+</script>
---
+++ day_21——Vue路由守卫/components/b_item.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'b_item',
+  data () {
+    return {
+      msg: 'bbb'
+    }
+  },
+  
+  
+}
+</script>
