40c84eaa039012b1f8c4c30322d493db1cca22eb
徐世成 <1213089093@qq.com>
2022-03-13 23:36:22 +0800

    feat(daily-vue): add isReactive file

--- daily-vue/响应式API/响应式基础API/isReactive.ts
+++ daily-vue/响应式API/响应式基础API/isReactive.ts
@@ -1,6 +1,6 @@
 // 检查对象是否是由 reactive 创建的响应式代理
 
-import { reactive, isReactive } from 'vue'
+import { reactive, isReactive, readonly } from 'vue'
 export default {
   setup() {
     const state = reactive({
@@ -8,6 +8,18 @@ export default {
       age: '男'
     })
     console.log(isReactive(state)) // true
+
+    // 从普通对象创建的只读 proxy
+    const plain = readonly({
+      name: 'Evildoer98',
+      age: 18
+    })
+
+    console.log(isReactive(plain)) // false
+
+    // 从响应式 proxy 创建的只读 proxy
+    const stateCopy = readonly(state)
+    console.log(isReactive(stateCopy)) // true
   }
 }
 
